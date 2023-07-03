77c89fba4972ad889fdd8c3d485ac90037e6868c
Vanson Leung <vanson@vanportdev.com>
2023-07-03 20:26:43 +0800

    Initial Commit

---
+++ SwiftUI-NavigationLinkBundle/SwiftUI_NavigationLinkBundleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavigationLinkBundleApp.swift
+//  SwiftUI-NavigationLinkBundle
+//
+//  Created by Leung Yu Wing on 3/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavigationLinkBundleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
